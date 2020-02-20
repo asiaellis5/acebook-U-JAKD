42e83ee5c6148b485128eacc8364889c6268ac8c
Asia <asiaellis5@gmail.com>
2020-02-20 18:24:36 +0000

    test: feature test for adding friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 class FriendshipsController < ApplicationController
-  skip_before_action  :authenticated_user
+  skip_before_action :authenticated_user
 
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
     if @friendship.save
-      flash[:notice] = "Friend Added"
-      redirect_to session[:url]
+      flash[:notice] = 'Friend Added'
     else
-      flash[:alert] = "Unable to add friend"
-      redirect_to session[:url]
+      flash[:alert] = 'Unable to add friend'
     end
+    redirect_to session[:url]
   end
 end
---
+++ spec/features/adding_friends_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+feature 'users can create a friendship' do
+  scenario 'from the users page' do
+    sign_up
+    create_post
+    click_link 'Sign Out'
+    sign_up_two
+    click_link 'Home'
+    click_link 'uvalente'
+    click_link 'Add Friend'
+    expect(page).to have_content('Friend Added')
+  end
+end
