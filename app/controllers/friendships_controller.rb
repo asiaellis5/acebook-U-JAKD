39741391f36db0427dba98f22fbbc5bfdd7ffffb
Asia <asiaellis5@gmail.com>
2020-02-20 18:18:28 +0000

    feat: adding friends create method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,14 @@
 class FriendshipsController < ApplicationController
   skip_before_action  :authenticated_user
+
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Friend Added"
+      redirect_to session[:url]
+    else
+      flash[:alert] = "Unable to add friend"
+      redirect_to session[:url]
+    end
+  end
 end
