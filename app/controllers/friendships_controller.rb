99083d8693126dadd1d36c328075c6e3c1398c52
Umberto Valente <uvalente89@gmail.com>
2020-02-20 18:52:25 +0000

    feat: add friend list view

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,6 +4,7 @@ class FriendshipsController < ApplicationController
   skip_before_action :authenticated_user
 
   def create
+    # ADD CHECK FOR CURRENT USER = USER AND PREVIOUS FRIENDSHIP -> BOTH WAYS
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
     if @friendship.save
       flash[:notice] = 'Friend Added'
@@ -11,5 +12,9 @@ class FriendshipsController < ApplicationController
       flash[:alert] = 'Unable to add friend'
     end
     redirect_to session[:url]
+  end
+
+  def show
+    @friendships = Friendship.all
   end
 end
